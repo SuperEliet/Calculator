2704b25c12b4bb76ed871ed8cf83601a85b3f665
Yvonne De La Peña <yvonnedelapena@gmail.com>
2015-06-23 16:20:32 -0400

    Create calculator.rb

---
+++ calculator.rb
@@ -0,0 +1,24 @@
+begin
+  puts "First number?"
+  first_number = gets.chomp.to_i
+  puts "Second number?"
+  second_number = gets.chomp.to_i
+  puts "Operation?"
+  operation = gets.chomp
+  if operation == '+'
+    puts "#{first_number}+#{second_number}="
+    puts "#{first_number+second_number}"
+  elsif operation == '-'
+    puts "#{first_number}-#{second_number}="
+    puts "#{first_number-second_number}"
+  elsif operation == '*'
+    puts "#{first_number}*#{second_number}="
+    puts "#{first_number*second_number}"
+  elsif operation == '/'
+    puts "#{first_number}/#{second_number}="
+    puts "#{first_number/second_number}"
+  end
+  puts "Quit? Y or N"
+  user_quit = gets.chomp
+end while user_quit != "Y"
+puts "Goodbye!"
